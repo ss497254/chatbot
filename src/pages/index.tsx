7d6dbe7b7aeb528471873b2fa128b25029a9a8ab
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-07-24 17:52:48 +0530

    fix: Chatbot folder text case

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ const HomePage: React.FC<HomePageProps> = () => {
   return (
     <div className="bg-gray-50 h-full">
       <h1 className="text-center py-10">Chatbot</h1>
+      <p className="text-center text-lg font-medium">
+        Click on the bottom right button to see magic!
+      </p>
       <ChatBot />
     </div>
   );
