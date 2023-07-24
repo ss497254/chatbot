1985662b0afca81129e6a398c964e55699387433
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-07-24 17:45:51 +0530

    refactor: chatbot

--- src/components/Chatbot/ChatBot.tsx
+++ src/components/Chatbot/ChatBot.tsx
@@ -11,14 +11,11 @@ export const ChatBot: React.FC<ChatBotProps> = () => {
   if (!isMounted) return null;
 
   return createPortal(
-    <div className="absolute bottom-10 right-10">
+    <div className="absolute bottom-6 right-6">
       {open && (
-        <div
-          className={["bg-white p-6 rounded-lg modal-container r"].join(" ")}
-          onClick={(e) => e.stopPropagation()}
-        ></div>
+        <div className="bg-white rounded-lg overflow-hidden shadow-lg h-[65vh] w-80 md:w-96 lg:w-[456px] absolute bottom-16 right-0"></div>
       )}
-      <button onClick={() => setOpen(open)}>
+      <button onClick={() => setOpen(!open)}>
         <img src="/logo.png" className="h-12 w-12" />
       </button>
     </div>,
