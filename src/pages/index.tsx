e0a5e800c86dcaff9d7af9fd915cf674559beeee
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-07-24 17:21:17 +0530

    update: home page and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,11 @@ import React from "react";
 interface HomePageProps extends React.PropsWithChildren {}
 
 const HomePage: React.FC<HomePageProps> = () => {
-  return <div className="bg-red-100 text-4xl font-semibold">Chatbot</div>;
+  return (
+    <div className="bg-gray-50 font-semibold h-full">
+      <h1 className="text-center my-20">Chatbot</h1>
+    </div>
+  );
 };
 
 export default HomePage;
