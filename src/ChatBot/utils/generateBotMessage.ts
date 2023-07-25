bbb290e7985df2b9b7431192492abba0da458b37
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-07-25 09:52:43 +0530

    feat: update generateBotMessage to type the message

--- src/ChatBot/utils/generateBotMessage.ts
+++ src/ChatBot/utils/generateBotMessage.ts
@@ -1,5 +1,22 @@
 import { useMessageStore } from "../stores/useMessagesStore";
 
 export const generateBotMessage = (content: string) => {
-  useMessageStore.getState().addMessage({ content, author: "Bot" });
+  const { addMessage, updateMessage } = useMessageStore.getState();
+  const words = content.split(" ");
+
+  const { timestamp } = addMessage({ content: "", author: "Bot" });
+
+  let i = 0;
+  const fn = () => {
+    updateMessage({
+      author: "Bot",
+      content: words.slice(0, i).join(" "),
+      timestamp,
+    });
+
+    i++;
+    setTimeout(fn, 150);
+  };
+
+  fn();
 };
